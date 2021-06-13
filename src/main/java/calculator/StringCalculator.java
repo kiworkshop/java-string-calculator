6f7b35311408f489c9660d348b9edd583992b7eb
eunii <eoeh04@gmail.com>
2021-06-13 12:22:13 +0900

    코드 정리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -8,36 +8,35 @@ import java.util.stream.Stream;
 public class StringCalculator {
 
     public static void main(String[] args) {
-        //String stringNumber = "//;\n1;2;3";
         String stringNumber = "1111,2";
-        //String stringNumber = "//;\n1;2;3";
         int sum = splitAndSum(stringNumber);
         System.out.println(sum);
     }
 
     public static int splitAndSum(String stringNumber) {
-        if(stringNumber == null || stringNumber.isEmpty()) {
-            return 0;
-        }
+        if(stringNumber == null || stringNumber.isEmpty())  return 0;
         int[] numbers = switchToNumbers(stringNumber);
+        checkValication(numbers);
+
         return IntStream.of(numbers).sum();
     }
 
     public static int[] switchToNumbers(String stringNumber){
         String customDelimiter = ",|:";
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(stringNumber);
+
         if (m.find()) {
             customDelimiter = m.group(1);
             stringNumber = m.group(2);
         }
+
         int[] numbers = Stream.of(stringNumber.split(customDelimiter)).mapToInt(Integer::parseInt).toArray();
-        checkValication(numbers);
+
         return numbers;
     }
 
     public static void checkValication(int[] numbers) {
         long countNegative = IntStream.of(numbers).filter(x -> x<0 ).count();
         if(countNegative>0) throw new RuntimeException();
-
     }
 }
